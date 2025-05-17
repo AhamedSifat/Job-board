12fa843df0046c488359a8d7b5114e66131122b7
sifat1234 <ahahmedsifat111@gmail.com>
2025-05-17 23:11:19 +0600

    Onboarding Flow

--- src/generated/prisma/index.js
+++ src/generated/prisma/index.js
@@ -184,7 +184,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": "../../../.env",
+    "rootEnvPath": null,
     "schemaEnvPath": "../../../.env"
   },
   "relativePath": "../../../prisma",
@@ -194,6 +194,7 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
+  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,6 +1,5 @@
 // lib/prisma.ts
-import { PrismaClient } from '@prisma/client';
-
+import { PrismaClient } from '@/generated/prisma';
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
 export const prisma = globalForPrisma.prisma || new PrismaClient();
